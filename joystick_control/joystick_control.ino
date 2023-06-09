857439ec4dfbfb6ce6aa6d813184c1942f297c42
cnst01 <cassionst@gmail.com>
2023-06-08 23:45:13 -0300

    controling face with joystick, no tests made

--- joystick_control/joystick_control.ino
+++ joystick_control/joystick_control.ino
@@ -1,3 +1,5 @@
+#import <Servo.h>
+
 struct {
   #define VRX A1
   #define VRY A0
@@ -8,18 +10,18 @@ struct {
   int last_but = 0;
   int last_x = 512;
   int last_y = 512;
-  void refresh(){
-    last_x = x;
-    last_y = y;
-    last_but = but;
-    x = analogRead(VRX);
-    y = analogRead(VRY);
-    but = digitalRead(Button);
-  }
   void setup(){
     pinMode(VRX, INPUT);
     pinMode(VRY, INPUT);
     pinMode(Button, INPUT_PULLUP);
+  }
+  void refresh(){
+    last_x = x;
+    last_y = y;
+    last_but = but;
+    x = int(analogRead(VRX)*100/1024);
+    y = int(analogRead(VRY)*100/1024);
+    but = digitalRead(Button);
   }
   int has_changes(int x_offset, int y_offset){
     int diff = x-last_x;
@@ -37,16 +39,64 @@ struct {
   }
 } joy;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  joy.setup();
-}
+struct{
+  Servo EyeY;
+  Servo EsqLid;
+  Servo DirLid;
+  Servo EsqEyeBrown;
+  void setup() {
+    // put your setup code here, to run once:
+    EyeX.attach(3);
+    EyeY.attach(4);
+    EsqLid.attach(8);
+    DirLid.attach(11);
+    Serial.begin(9600);
+    EyeX.write(30);
+    EyeY.write(50);
+    EsqLid.write(15);
+    DirLid.write(40);
+  }
+  void move_to_default(){
+    EyeX.write(30);
+    EyeY.write(50);
+    EsqLid.write(15);
+    DirLid.write(40);
+  }
+  void blink(){
+    Serial.println("2 eyes blinking");
+    EsqLid.write(40);
+    DirLid.write(1);
+    delay(100);
+    move_to_default(); 
+  }
+  void move_eyes(int x,int y){
+    EyeX.write(0.6*x);
+    EyeY.write((0.6*y) + 40);
+  }
+}face;
 
-void loop() {
+/*
+eixo y dos olhos varia de 40 a 100
+eixo x 1 a 60
+esq fechado = 1 aberto = 40
+dir fechado = 50 aberto = 1
+
+*/
+
+
+void Face_control(){
+  joy.refresh();
+  if(joy.has_changes(10,10)){
+    face.move_eyes(joy.x, joy.y);
+    if(!joy.but){
+      face.blink();
+    }
+  }
+}
+void testing_joy(){
   // put your main code here, to run repeatedly:
   joy.refresh();
-  if(joy.has_changes(10, 10) == 1){
+  if(joy.has_changes(10, 10)){
     Serial.print("X: ");
     Serial.print(joy.x);
     Serial.print(" | Y: ");
@@ -54,4 +104,14 @@ void loop() {
     Serial.print("Button: ");
     Serial.println(joy.but);
   }
+}
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  joy.setup();
+  face.setup();
+}
+
+void loop() {
+  Face_control();    
 }
